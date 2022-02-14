15b7302fd06aca220aefaf6320d4a0606c4f3ac3
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-02-14 19:14:08 +0100

    Add files via upload

---
+++ PracticalWeek9/CP2406 Week9/src/ProgrammingExercises/JColorFrame.java
@@ -0,0 +1,62 @@
+package ProgrammingExercises;
+
+import javafx.scene.layout.BorderWidths;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.Color;
+public class JColorFrame extends JFrame implements ActionListener
+{
+    Button colorButton = new Button("Change Border Color");
+    JPanel CenterPanel = new JPanel();
+    JPanel NorthPanel = new JPanel();
+    JPanel SouthPanel = new JPanel();
+    JPanel EastPanel = new JPanel();
+    JPanel WestPanel = new JPanel();
+    int count = 0;
+    public JColorFrame()
+    {
+        super("JColorFrame");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container con = this.getContentPane();
+        con.setLayout(new BorderLayout());
+        CenterPanel.add(colorButton);
+        con.add(NorthPanel, BorderLayout.NORTH);
+        con.add(SouthPanel, BorderLayout.SOUTH);
+        con.add(EastPanel, BorderLayout.EAST);
+        con.add(WestPanel, BorderLayout.WEST);
+        con.add(CenterPanel, BorderLayout.CENTER);
+        colorButton.addActionListener(this);
+    }
+    public static void main(String[] args)
+    {
+        JColorFrame cFrame = new JColorFrame();
+        cFrame.setSize(350,250);
+        cFrame.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if(count == 0){
+            NorthPanel.setBackground(Color.GREEN);
+            count += 1;}
+        else if(count==1){
+            EastPanel.setBackground(Color.YELLOW);
+            count += 1;}
+        else if(count==2){
+            WestPanel.setBackground(Color.BLUE);
+            count += 1;}
+        else if(count==3){
+            SouthPanel.setBackground(Color.RED);
+            count += 1;}
+        else
+        {
+            EastPanel.setBackground(null);
+            SouthPanel.setBackground(null);
+            NorthPanel.setBackground(null);
+            WestPanel.setBackground(null);
+            count = 0;
+        }
+    }
+}
---
+++ PracticalWeek9/CP2406 Week9/src/ProgrammingExercises/ReadRandomEmployeeFile.java
@@ -0,0 +1,116 @@
+package ProgrammingExercises;
+
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class ReadRandomEmployeeFile extends JFrame
+        implements ActionListener
+{
+    private JLabel title =
+            new JLabel("Employee data");
+    Font bigFont = new Font("Helvetica", Font.ITALIC, 24);
+    private JTextField idField = new JTextField(4);
+    private JLabel idLabel = new JLabel("ID");
+    private JButton RetrieveDataButton =
+            new JButton("retrieve data");
+    JPanel InfoPanel = new JPanel();
+
+    private JLabel EmployeeLabelID1 = new JLabel("ID Number:");
+    private JLabel EmployeeLabelFirst1 = new JLabel("First Name:");
+    private JLabel EmployeeLabelLast1 = new JLabel("Last Name:");
+    private JLabel EmployeeLabelPR1 = new JLabel("Pay Rate: ");
+
+    private JLabel EmployeeLabelID = new JLabel("");
+    private JLabel EmployeeLabelFirst = new JLabel("");
+    private JLabel EmployeeLabelLast = new JLabel("");
+    private JLabel EmployeeLabelPR = new JLabel("");
+
+    private Container con = getContentPane();
+    BufferedReader br;
+
+    public ReadRandomEmployeeFile()
+    {
+        setSize(280, 200);
+        con.setLayout(new FlowLayout());
+        title.setFont(bigFont);
+        con.add(title);
+        con.add(idLabel);
+        con.add(idField);
+        con.add(RetrieveDataButton);
+        RetrieveDataButton.addActionListener(this);
+
+        InfoPanel.setLayout(new GridLayout(4,2));
+        InfoPanel.add(EmployeeLabelID1);
+        InfoPanel.add(EmployeeLabelID);
+        InfoPanel.add(EmployeeLabelFirst1);
+        InfoPanel.add(EmployeeLabelFirst);
+        InfoPanel.add(EmployeeLabelLast1);
+        InfoPanel.add(EmployeeLabelLast);
+        InfoPanel.add(EmployeeLabelPR1);
+        InfoPanel.add(EmployeeLabelPR);
+
+        con.add(InfoPanel);
+
+
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e1)
+    {
+        try
+        {
+            br = new BufferedReader(new FileReader("Employees.txt"));
+        }
+        catch(IOException e)
+        {
+            System.err.println("File not opened");
+            System.exit(1);
+        }
+
+        String IDnum;
+        try
+        {
+            IDnum = idField.getText();
+            String line = br.readLine();
+
+            while (line != null) {
+                if(IDnum.equals(line)){
+                    EmployeeLabelID.setText("   " + line);
+                    line = br.readLine();
+                    EmployeeLabelFirst.setText("   " + line);
+                    line = br.readLine();
+                    EmployeeLabelLast.setText("   " + line);
+                    line = br.readLine();
+                    EmployeeLabelPR.setText("   " + "$" + line);
+                    break;
+                }
+                line = br.readLine();
+            }
+            if (line == null){
+                EmployeeLabelID.setText("ID does not exist");
+                EmployeeLabelFirst.setText("");
+                EmployeeLabelLast.setText("");
+                EmployeeLabelPR.setText("");
+            }
+            br.close();
+
+
+        }
+        catch(IOException e)
+        {
+            System.err.println("File not opened");
+            System.exit(1);
+        }
+
+
+
+
+
+    }
+    public static void main(String[] args)
+    {
+        ReadRandomEmployeeFile file = new ReadRandomEmployeeFile();
+    }
+}
