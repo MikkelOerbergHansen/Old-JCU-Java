cc9a8ce3f1e71d19eda6a40cb95da59ec3e7c9eb
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-02-14 19:13:33 +0100

    Add files via upload

---
+++ PracticalWeek8/CP2406 Week8/src/ProgrammingExercises/JBookQuote.java
@@ -0,0 +1,30 @@
+package ProgrammingExercises;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class JBookQuote extends JFrame
+{
+    FlowLayout flow = new FlowLayout();
+    JLabel msg1 = new  JLabel("Fear is the path to the dark side. ");
+    JLabel msg2 = new JLabel("Fear leads to anger;");
+    JLabel msg3 = new JLabel("anger leads to hate; ");
+    JLabel msg4 = new JLabel("hate leads to suffering.");
+    public JBookQuote()
+    {
+        super("Book Quote");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(flow);
+        add(msg1);
+        add(msg2);
+        add(msg3);
+        add(msg4);
+    }
+    public static void main(String[] args)
+    {
+        JBookQuote aFrame = new JBookQuote();
+        aFrame.setSize(300, 150);
+        aFrame.setVisible(true);
+    }
+}
