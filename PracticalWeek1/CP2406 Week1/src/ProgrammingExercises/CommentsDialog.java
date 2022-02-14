3e4120609eb7872a56cd6403ef40df487512b062
MikkelOerbergHansen <99221426+MikkelOerbergHansen@users.noreply.github.com>
2022-02-14 19:07:36 +0100

    Add files via upload

---
+++ PracticalWeek1/CP2406 Week1/src/ProgrammingExercises/CommentsDialog.java
@@ -0,0 +1,12 @@
+package ProgrammingExercises;
+/* comments */
+
+import javax.swing.JOptionPane;
+public class CommentsDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(
+                null,
+                "'Program comments are non-executing statements you add to a file " +
+                        " for the purpose of documentation'");
+    }
+}
---
+++ PracticalWeek1/CP2406 Week1/src/ProgrammingExercises/TablesAndChairs.java
@@ -0,0 +1,13 @@
+package ProgrammingExercises;
+/* comments */
+
+public class TablesAndChairs {
+    public static void main(String[] args) {
+        System.out.println("X                       X");
+        System.out.println("X                       X");
+        System.out.println("X      XXXXXXXXXX       X");
+        System.out.println("XXXXX  X        X  XXXXXX");
+        System.out.println("X   X  X        X  X    X");
+        System.out.println("X   X  X        X  X    X");
+    }
+}
